d7c78c49fa5a2cefd2418264379d652931c8cadf
DouglasDGiT <143660640+DouglasDGiT@users.noreply.github.com>
2024-08-21 18:22:59 -0300

    Update Login.jsx
    Finalizando envio de dados

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -2,15 +2,23 @@ import React from 'react'
 import { useState } from 'react';
 import "./Login.css";
 const Login = () => {
+  const[username, setUsername] = useState("");
+  const[password, setPassword] = useState("");
+
+  const handleSubmit =  (event) => {
+    event.preventDefault();
+    console.log("Envio");
+    alert("Para Teste "+username +"  -  "+ password); 
+  }
   return (
     <div className="container">
       <input type="checkbox" id="check" />
       <div className="login form">
         <header>LOGIN</header>
 
-        <form method="POST" action="/estacionamento/Perfil">
-          <input type="text" id="email" placeholder="Email" name="email" />
-          <input type="password" id="senha" placeholder="Senha" name="senha" />
+        <form onSubmit={handleSubmit} method="POST" action="/estacionamento/Perfil">
+          <input type="email" id="email" placeholder="Email" name="email" onChange={(e) => setUsername(e.target.value)}/>
+          <input type="password" id="senha" placeholder="Senha" name="senha" onChange={(e) => setPassword(e.target.value)} />
           <input type="submit" className="button" value="Entrar" />
         </form>
 
